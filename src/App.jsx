664be686bc5fa6151a2c6df1a384ade14d3dc593
Otavio Neri <otavioneri47@gmail.com>
2022-08-17 17:29:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import {useState} from 'react'
+import {FiSearch} from 'react-icons/fi'
+import './App.css'
+import api from './components/axios/api'
+
+
+function App() {
+  const  [input , setInput] = useState ('');
+  const [cep , setCep] = useState ([])
+
+
+  async function handleSearch  () {
+    
+    if (input === ''){
+      alert('DIGITE ALGUM CEP!')
+    } 
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+
+    
+    } catch {
+      alert('OPS, CEP NÃO ENCONTRADO!');
+      setInput('')
+    }
+
+  }
+  return (  
+    <div className="App">
+
+      <div className='cabeca'>
+        <h2 className='title'>Buscador CEP</h2>
+        <div className='conteinerInput'>
+          <input className='inputCep'
+          type="text"
+          placeholder='Digite seu CEP...' 
+          value={input}
+          onChange={(e) => setInput(e.target.value) }
+            />
+
+          <button className='buttonsearch' onClick={handleSearch}>
+            <FiSearch size={25}/>
+          </button>
+        </div>
+      </div >
+      {Object.keys(cep).length > 0 && (
+        <main className='card'>
+          <h2>{`CEP : ${cep.cep}`}</h2>
+          <span>{cep.logradouro}</span>
+          <span>{`${cep.complemento}`}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+        
+    </div>
+  )
+}
+
+export default App
